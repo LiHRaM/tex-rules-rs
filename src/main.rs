018375ccfdba4135e2c2fd0175901fe2a967fa9e
Hilmar Gústafsson <hilmargustafs@gmail.com>
2020-02-28 11:43:17 +0100

    Add regression test

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,30 @@ use rayon::prelude::*;
 
 mod lib;
 fn main() -> Result<()> {
-    let errored = lib::find_tex_files()
-        .collect::<Vec<_>>()
-        .par_iter()
-        .map(|path| lib::parse_from_path(&path).is_err())
-        .reduce(|| false, |acc, elem| acc || elem);
+    let errored = parse_from_current_path();
 
     if !errored {
         Ok(())
     } else {
         Err(anyhow::anyhow!("Errors were detected."))
+    }
+}
+
+fn parse_from_current_path() -> bool {
+    lib::find_tex_files()
+        .collect::<Vec<_>>()
+        .par_iter()
+        .map(|path| lib::parse_from_path(&path).is_err())
+        .reduce(|| false, |acc, elem| acc || elem)
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn reg_error_found() {
+        let errored = parse_from_current_path();
+        assert_eq!(errored, true);
     }
 }
